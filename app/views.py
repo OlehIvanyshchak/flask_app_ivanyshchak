0e6018d27631c6b2f9fc6c3c2c7f12eac38ddaca
OlehIvanyshchak <oleh.ivanyshchak.22@pnu.edu.ua>
2024-12-15 16:59:44 +0200

    Add user blueprint

--- app/views.py
+++ app/views.py
@@ -1,31 +1,17 @@
-from flask import request, url_for, redirect, render_template
+from flask import request, render_template
 from . import app
 
 
 @app.route("/")
 def main():
-    return "Hello, World!"
+    return render_template("hello.html")
 
 
 @app.route("/homepage")
 def home():
     """View foe the Home page of your website"""
     agent = request.user_agent
-    return f"This is your Home page - {agent}"
-
-
-@app.route("/user/<string:name>/<int:age>")
-def greeting(name, age):
-    name = name.upper()
-    age = request.args.get("age", 0, int)
-    return render_template("hi.html", name=name, age=age)
-
-
-@app.route("/admin")
-def admin():
-    to_url = url_for("greeting", name="administrator", age=30, _external=True)
-    print(to_url)
-    return redirect(to_url)
+    return render_template("home.html", agent=agent)
 
 
 @app.route("/resume")
